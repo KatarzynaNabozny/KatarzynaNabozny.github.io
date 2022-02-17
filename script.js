dea2cf09b22a43002e7521a1b5bf96cebf4d4450
Katarzyna Nabożny <knnabozny@gmail.com>
2022-02-17 21:40:57 +0100

    change to https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://kodilla-tasks-kns.herokuapp.com/v1/task';
+  var apiRoot = 'https://kodilla-tasks-kns.herokuapp.com/v1/task';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
