b8905e1eabfaf3aab6a5a07f2ef1ad50448962f1
Katarzyna Nabożny <knnabozny@gmail.com>
2022-02-19 19:32:01 +0100

    Refactor

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://kodilla-tasks-kns.herokuapp.com/v1/task';
+  var apiRoot = 'https://kodilla-tasks-kns.herokuapp.com/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
